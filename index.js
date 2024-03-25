0caf9b4eec11a6e43fb4468571127a2f588a331f
jubairskv <jubairskv@gmail.com>
2024-03-25 20:47:19 +0530

    map,filter,reduec code

--- index.js
+++ index.js
@@ -166,4 +166,21 @@ const output6=arr.filter((x)=>x<3)*/}
 
 //REDUCE------------------------------------------------------
 
-const b=[5,1,3,2,6]  
+const arr=[5,1,3,2,6]  
+
+//sum or max
+
+function findSum(arr){
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        sum=sum+arr[i]
+    }
+    return sum
+}
+console.log(findSum(arr))
+
+const output8=arr.reduce(function(acc,curr){
+    acc=acc+curr
+    return acc
+},0)
+console.log(output8)
