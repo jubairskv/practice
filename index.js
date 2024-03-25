02ba6ba3a93f38599a30b7b32d583f8ec0703ef6
jubairskv <jubairskv@gmail.com>
2024-03-25 20:40:27 +0530

    map,filter,reduec code

--- index.js
+++ index.js
@@ -110,3 +110,60 @@ console.log(calu(radiuss,circumference))
 console.log(calu(radiuss,diameter))
 */}
 //------------------------------------------------------------------
+//map,reduce,filter
+{/* 
+const arr=[5,1,3,2,6]   //map iterate over an array and it returns nre array
+
+//double-[10,2,6,2,12]
+//triple-[15,3,9,6,18]
+//binary-["101","1","11","10","110"]
+
+function double(x){
+   return x*2
+}
+
+function triple(x){
+    return x*3
+ }
+
+ function binary(x){
+    return x.toString(2)
+ }
+
+const output = arr.map(function binary(x){     //higher order function
+    return x.toString(2)
+ });
+
+ const output2 = arr.map((x)=>{     //arrow  function
+    return x.toString(2)
+ });
+
+ const output3 = arr.map((x)=> x.toString(2));  //optimizing code
+console.log(output)
+*/}
+
+//FILTER--------------------------------------------
+{/* 
+const a=[5,1,3,2,6]        //filter used to filter specific value in the array
+
+function isOdd(x){
+    return x%2
+}
+
+function isEven(x){
+    return x%2===0
+}
+
+function gearterThan4(x){
+    return x>4;
+}
+
+const output4 =a.filter(isOdd)
+const output5 =a.filter(function gearterThan4(x){
+    return x>4;
+})
+const output6=arr.filter((x)=>x<3)*/}
+
+//REDUCE------------------------------------------------------
+
+const b=[5,1,3,2,6]  
